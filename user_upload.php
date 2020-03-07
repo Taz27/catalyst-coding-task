1bc9a12be0946972a3bc583411d1994dc8221ab5
Taran Mand <tsmand@yahoo.com>
2020-03-07 15:18:10 +1100

    read CSV file and print

--- user_upload.php
+++ user_upload.php
@@ -1,14 +1,5 @@
 <?php
-//testing command line arguments
-
-if (isset($argc)) {
-	for ($i = 0; $i < $argc; $i++) {
-		echo "Argument #" . $i . " - " . $argv[$i] . "\n";
-	}
-}
-else {
-	echo "argc and argv disabled\n";
-}
+//testing command line arguments using 'getopt' function
 
 $shortopts = "u:p:h:";
 $longopts = array("file:", "create_table", "dry_run");
@@ -16,5 +7,20 @@ $longopts = array("file:", "create_table", "dry_run");
 $options = getopt($shortopts, $longopts);
 var_dump($options);
 //print_r($options);
+
+// Open the CSV file passed as argument for reading and print line by line
+if (($h = fopen("{$options['file']}", "r")) !== FALSE) {
+  
+    while (($data = fgetcsv($h, 1000, ",")) !== FALSE) 
+    {		
+        if ($data[0] != null) {
+            // Read the data from a single line
+            echo "$data[0] $data[1] $data[2] \n";
+        }
+        
+    }
+    // Close the file
+    fclose($h);
+} 
 
 ?>
